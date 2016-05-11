a1aa3cbc7d43e5cc7aa686bda57e02117fdaf755
khayapro <kmathebula@fnb.co.za>
2016-05-12 00:11:36 +0200

    floating and modulus demo

--- ex3.py
+++ ex3.py
@@ -1,11 +1,14 @@
 print "I will now count the remaining planets... before Darth Vader comes back!"
 
+# Counting the number of planents below
 print "Earth", 25 + 30 / 6
 print "Jupiter", 100 - 23 * 3 % 4
 
+# Counting the number of stars below
 print "Now I will count the stars"
 print 3 + 5 + 1 + - 5 + 2 % 2 - 1 / + 5
 
+# Checking a boolean on the below
 print "is it true that 3 + 2 < 5 - 7?"
 print 3 + 2 < 5 - 7
 
@@ -14,3 +17,9 @@ print "What is 5 - 7?", 5 - 7
 
 print "How about some more?"
 print "No, this is all for today..."
+
+# Floating number below with python, doesnt drop the fraction
+print 5.0 / 2.0
+
+# Modulus demostration, the below should give a 0 remainder.
+print 100 / 10 % 5
