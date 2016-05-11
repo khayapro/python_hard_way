42bbf58e7baf5d96c56dfa74c4f0d88a1a68ff80
khayapro <kmathebula@fnb.co.za>
2016-05-12 00:02:17 +0200

    counting with python

---
+++ ex3.py
@@ -0,0 +1,16 @@
+print "I will now count the remaining planets... before Darth Vader comes back!"
+
+print "Earth", 25 + 30 / 6
+print "Jupiter", 100 - 23 * 3 % 4
+
+print "Now I will count the stars"
+print 3 + 5 + 1 + - 5 + 2 % 2 - 1 / + 5
+
+print "is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "How about some more?"
+print "No, this is all for today..."
